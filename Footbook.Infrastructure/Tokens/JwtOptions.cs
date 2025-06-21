0c717d6b53680e6570a58e3b6a0472cac84a411b
fakhri2406 <Gezal_yv57@itstep.edu.az>
2025-06-21 13:31:32 +0400

    refactor: suppress nullable warnings with '= null!'

--- Footbook.Infrastructure/Tokens/JwtOptions.cs
+++ Footbook.Infrastructure/Tokens/JwtOptions.cs
@@ -4,8 +4,8 @@ namespace Footbook.Infrastructure.Tokens;
 
 public class JwtOptions
 {
-    public string Issuer { get; set; }
-    public string Audience { get; set; }
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
     public TimeSpan AccessValidFor { get; set; }
-    public SigningCredentials SigningCredentials { get; set; }
+    public SigningCredentials SigningCredentials { get; set; } = null!;
 }
